fdf11f15d8ef08c19bbecb312c37c5c34d5065c9
build2stone <seggers@protonmail.com>
2020-02-25 13:04:04 +0100

    Revert cursor colour

--- config.h
+++ config.h
@@ -118,7 +118,7 @@ static const char *colorname[] = {
 	"#fbf1c7",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
+	"#d5c4a1", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
 	"#282828", /* 258 -> bg */
 	"#ebdbb2", /* 259 -> fg */
